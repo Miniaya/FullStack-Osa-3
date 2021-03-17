8d0fcd02e32a1c32d01372e1704bb453183314a3
Henna Mäkiluoma <henna.makiluoma@gmail.com>
2021-03-17 23:02:28 +0200

    uibuild

--- index.js
+++ index.js
@@ -20,34 +20,56 @@ app.get('/api/persons', (req, res) => {
 })
 
 app.get('/info', (req, res) => {
-    res.send(
-        `<p>Phonebook has info for ${persons.length} people</p>
-         <p>${new Date()}</p>`
-    )
+    Person.collection.countDocuments()
+        .then(count => {
+            res.send(
+                `<p>Phonebook has info for ${count} people</p>
+                 <p>${new Date()}</p>`
+            )
+        })
 })
 
-app.get('/api/persons/:id', (req, res) => {
-    Person.findById(req.params.id).then(person => {
-        response.json(person)
-    })
+app.get('/api/persons/:id', (req, res, next) => {
+    Person.findById(req.params.id)
+        .then(person => {
+            if(person) {
+                res.json(person)
+            } else {
+                res.status(404).end()
+            }
+        })
+        .catch(error => next(error))
 })
 
-app.delete('/api/persons/:id', (req, res) => {
-    const id = Number(req.params.id)
-    persons = persons.filter(p => p.id !== id)
+app.delete('/api/persons/:id', (req, res, next) => {
+    Person.findByIdAndRemove(req.params.id)
+        .then(result => {
+            res.status(204).end()
+        })
+        .catch(error => next(error))
+})
 
-    res.status(204).end()
+app.put('/api/persons/:id', (req, res, next) => {
+    const body = req.body
+
+    const person = {
+        name: body.name,
+        number: body.number,
+    }
+
+    Person.findByIdAndUpdate(req.params.id, person, {new: true})
+        .then(updatedPerson => {
+            res.json(updatedPerson)
+        }) 
+        .catch(error => next(error))
 })
 
 app.post('/api/persons', (req, res) => {
     const body = req.body
 
-    console.log(body.name)
-    console.log(body.number)
-
-    if(body.name === undefined || body.number === undefined) {
+    if (body.name === undefined || body.number === undefined) {
         return res.status(400).json({error: 'name or number missing'})
-    } 
+    }
 
     const person = new Person({
         name: body.name,
@@ -58,6 +80,16 @@ app.post('/api/persons', (req, res) => {
         res.json(savedPerson)
     })
 })
+
+const errorHandler = (error, req, res, next) => {
+    console.error(error.message)
+
+    if (error.name === 'CastError') {
+        return res.status(400).send({ error: 'malformatted id' })
+    }
+
+    next(error)
+}
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
