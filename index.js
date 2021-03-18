9385d689df76d5fc862c130a0ba37f4de0887b0c
Henna Mäkiluoma <henna.makiluoma@gmail.com>
2021-03-18 15:47:16 +0200

    uibuild

--- index.js
+++ index.js
@@ -64,21 +64,19 @@ app.put('/api/persons/:id', (req, res, next) => {
         .catch(error => next(error))
 })
 
-app.post('/api/persons', (req, res) => {
+app.post('/api/persons', (req, res, next) => {
     const body = req.body
-
-    if (body.name === undefined || body.number === undefined) {
-        return res.status(400).json({error: 'name or number missing'})
-    }
 
     const person = new Person({
         name: body.name,
         number: body.number,
     })
 
-    person.save().then(savedPerson => {
-        res.json(savedPerson)
-    })
+    person
+        .save()
+        .then(savedPerson => savedPerson.toJSON())
+        .then(formattedPerson => res.json(formattedPerson))
+        .catch(error => next(error))
 })
 
 const errorHandler = (error, req, res, next) => {
@@ -86,6 +84,8 @@ const errorHandler = (error, req, res, next) => {
 
     if (error.name === 'CastError') {
         return res.status(400).send({ error: 'malformatted id' })
+    } else if (error.name === 'ValidationError') {
+        return res.status(400).json({ error: error.message })
     }
 
     next(error)
